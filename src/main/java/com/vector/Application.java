d4be73ee4df5299d431e2aac15cde725c9310a1a
vector <295253797@qq.com>
2018-07-26 19:56:08 +0800

    searchhouse storm

---
+++ src/main/java/com/vector/Application.java
@@ -0,0 +1,14 @@
+package com.vector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 12345 on 2018-7-25.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
